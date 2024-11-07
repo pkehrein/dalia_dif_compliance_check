11a14d6898bde2837aca4464c8f51b179f55dd38
Paul Kehrein <paul.kehrein@stud.h-da.de>
2024-11-07 17:10:40 +0100

    Added check for communities, some minor refactorings

--- check_csv.py
+++ check_csv.py
@@ -5,14 +5,24 @@ import json
 global_header_lines = 0
 global_line_offset = 2
 
+
 def read_license_file():
     # Reads the license data from its JSON-file and calls the extract_identifier-function.
-    # Expects: None
+    # Expects: None.
     # Returns: A list of strings.
     with open("licenses.json", 'r') as file:
         data = json.load(file)
     license_list = extract_identifier(data['licenses'])
     return license_list
+
+
+def read_communities_list():
+    # Reads the communities list from its JSON-file.
+    # Expects: None.
+    # Returns: A list of strings.
+    with open("communities.json", 'r') as file:
+        data = json.load(file)
+    return data
 
 
 def extract_identifier(license_data):
@@ -54,6 +64,13 @@ def fill_empyt_cells(data_frame):
     data_frame.fillna("", inplace=True)
 
 
+def split_into_list(string):
+    # Splits the given string at the delimiter '*' into list elements.
+    # Expects: A string containing at least two items split by a '*'.
+    # Returns: A list of strings.
+    return re.split("\s\*\s", string)
+
+
 def check_authors(authors):
     # Checks the column 'Authors' according to the rules of the DIF.
     # Expects: A series from a pandas dataframe containing the authors strings.
@@ -61,24 +78,27 @@ def check_authors(authors):
     author_errors = []
     for index, author in enumerate(authors):
         if author is "":
-            author_errors.append(f"Line {index + global_header_lines + global_line_offset}: Mandatory attribute 'Author' is missing.")
+            author_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: Mandatory attribute 'Author' is missing.")
             continue
         if '\*' in author:
-            author_list = re.split("\s\*\s", author)
+            author_list = split_into_list(author)
             for auth in author_list:
-                if check_if_error(auth):
+                if check_author_format(auth):
                     author_errors.append(f"Line {index + global_header_lines + global_line_offset}: Wrong name format.")
         else:
-            if check_if_error(author):
+            if check_author_format(author):
                 author_errors.append(f"Line {index + global_header_lines + global_line_offset}: Wrong name format.")
     return author_errors
 
 
-def check_if_error(author):
+def check_author_format(author):
     # Check the format of a name with a regular expression.
     # Expects: A string.
     # Returns: False if the string matches the constraints of the DIF and True if it doesnt.
-    if re.search("^[A-Za-zäöüÄÖÜß]*,\s[A-Za-zäöüÄÖÜß]*$", author) is not None or re.search("[A-Za-zäöüÄÖÜß]*,\s[A-Za-zäöüÄÖÜß]*\s?:\s?\{\S*}$", author) is not None or re.search("^[A-Za-z0-9äöüÄÖÜß]*\s:\s\{organization", author) is not None or re.search("^n/a$", author) is not None:
+    if re.search("^[A-Za-zäöüÄÖÜß]*,\s[A-Za-zäöüÄÖÜß]*$", author) is not None or re.search(
+            "[A-Za-zäöüÄÖÜß]*,\s[A-Za-zäöüÄÖÜß]*\s?:\s?\{\S*}$", author) is not None or re.search(
+            "^[A-Za-z0-9äöüÄÖÜß]*\s:\s\{organization", author) is not None or re.search("^n/a$", author) is not None:
         return False
     else:
         return True
@@ -96,7 +116,8 @@ def check_licenses(licenses):
             license_errors.append(f"Line {index + global_header_lines + global_line_offset}: License is missing.")
             continue
         if license_id not in license_list:
-            license_errors.append(f"Line {index + global_header_lines + global_line_offset}: Provided License is not part of the list from 'https://spdx.org/licenses/' or is in a wrong format.")
+            license_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: Provided License is not part of the list from 'https://spdx.org/licenses/' or is in a wrong format.")
     return license_errors
 
 
@@ -112,8 +133,10 @@ def check_link(link_list):
             link_errors.append(f"Line {index + global_header_lines + global_line_offset}: Link is missing.")
             continue
         if re.search("^https://\S*$", link) is None:
-            link_errors.append(f"Line {index + global_header_lines + global_line_offset}: Link is not in a valid format.")
+            link_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: Link is not in a valid format.")
     return link_errors
+
 
 def check_title(titles):
     # Checks the column 'Title' according to the rules of the DIF.
@@ -135,8 +158,61 @@ def check_description(descriptions):
 
     for index, description in enumerate(descriptions):
         if description is "":
-            description_errors.append(f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide a description for a resource.")
+            description_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide a description for a resource.")
     return description_errors
+
+
+def check_community_format(string):
+    # Checks the community string formatting.
+    # Expects: A string.
+    # Returns: If string is of format 'Community (S|R|RS) False or no error and if not True or error found.
+    if re.search('^[A-Za-z0-9äöüÄÖÜß]*\s\((RS|SR|S|R)\)$', string) is not None:
+        return False
+    else:
+        return True
+
+
+def check_community_name(string):
+    # Checks if the community name is in the list of community ids.
+    # Expects: A string in the format 'Community ()'
+    # Returns: True if in list, false if not.
+    communities_list = read_communities_list()
+    community_name = re.sub('\s\(\S*\)', '', string)
+    if community_name in communities_list["subsidiaries"]:
+        return True
+    else:
+        return False
+
+
+def check_community(communities):
+    # Checks the column 'Community' according to the rules of the DIF.
+    # Expects: A series from a pandas dataframe containing the supporting or recommending communities.
+    # Returns: A list of errors with the line number and type of error.
+    community_errors = []
+
+    for index, community in enumerate(communities):
+        if community is "":
+            community_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide a Community, either as supporting or recommending entity.")
+        else:
+            if "\*" in community:
+                community_list = split_into_list(community)
+                for com in community_list:
+                    community_errors = check_single_community(com, community_errors, index)
+            else:
+                community_errors = check_single_community(community, community_errors, index)
+    return community_errors
+
+
+def check_single_community(com, community_errors, index):
+    if check_community_format(com):
+        community_errors.append(
+            f"Line {index + global_header_lines + global_line_offset}: The provided format for the community contains errors.")
+    if not check_community_name(com):
+        community_errors.append(
+            f"Line {index + global_header_lines + global_line_offset}: The provided name does not match any name from the subsidiaries list. Please check if the name is correct. If it is you can ignore this warning.")
+    return community_errors
 
 
 def check_data(dataframe):
@@ -169,6 +245,12 @@ def check_data(dataframe):
         found_errors['Description'] = check_description(dataframe['Description'])
     else:
         found_errors['Description'] = "It is recommended to provide a description for every item!"
+
+    if dataframe['Community'] is not None:
+        found_errors['Community'] = check_community(dataframe['Community'])
+    else:
+        found_errors[
+            'Community'] = "It is recommended to provide information about recommending and supporting commnities!"
 
     return found_errors
 
