d0b6bb81e56ea81df71425d6f061d8364dde4eb8
Paul Kehrein <paul.kehrein@stud.h-da.de>
2025-03-04 11:19:54 +0100

    discipline, mediatype, proficiency level, publication date and file format check done

--- check_csv.py
+++ check_csv.py
@@ -4,6 +4,8 @@ import json
 
 global_header_lines = 0
 global_line_offset = 2
+global_media_types = ["audio", "video", "text", "presentation", "code", "image", "multipart"]
+global_proficiency_levels = ["novice", "advanced beginner", "competent", "proficient", "expert"]
 
 
 def read_license_file():
@@ -23,6 +25,22 @@ def read_communities_list():
     with open("communities.json", 'r') as file:
         data = json.load(file)
     return data
+
+
+def read_data_formats_file():
+    with open("mimeData.json", 'r') as file:
+        data = json.load(file)
+    file_formats_list = extract_file_types(data)
+    return file_formats_list
+
+
+def extract_file_types(file_format_list):
+    file_types = []
+    for file_formats in file_format_list:
+        file_format_types = file_formats["fileTypes"]
+        for file_format in file_format_types:
+            file_types.append(file_format)
+    return file_types
 
 
 def extract_identifier(license_data):
@@ -214,6 +232,123 @@ def check_single_community(com, community_errors, index):
             f"Line {index + global_header_lines + global_line_offset}: The provided name does not match any name from the subsidiaries list. Please check if the name is correct. If it is you can ignore this warning.")
     return community_errors
 
+def check_disciplines(disciplines):
+    # Checks the column 'Discipline' according to the rules of the DIF.
+    # Expects: A series from a pandas dataframe containing links to the relevant disciplines listed in https://skohub.io/dini-ag-kim/hochschulfaechersystematik/heads/master/w3id.org/kim/hochschulfaechersystematik/scheme.html
+    # Returns: A list of errors with the line number and type of error.
+    disciplines_errors = []
+
+    for index, discipline in enumerate(disciplines):
+        if discipline is "":
+            disciplines_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide at least one relevant discipline as a link listed in https://skohub.io/dini-ag-kim/hochschulfaechersystematik/heads/master/w3id.org/kim/hochschulfaechersystematik/scheme.html .")
+        else:
+            if "\*" in discipline:
+                discipline_list = split_into_list(discipline)
+                for disc in discipline_list:
+                    disciplines_errors = check_single_discipline(disc, disciplines_errors, index)
+            else:
+                disciplines_errors = check_single_discipline(discipline, disciplines_errors, index)
+    return disciplines_errors
+
+
+def check_single_discipline(discipline, discipline_errors, index):
+    if check_discipline_link(discipline):
+        discipline_errors.append(
+            f"Line {index + global_header_lines + global_line_offset}: The provided format is not of the type xsd:anyURI.")
+
+    return discipline_errors
+
+
+def check_discipline_link(discipline):
+    if re.search("^https://w3id\.org/kim/hochschulfaechersystematik/n\d+", discipline) is not None:
+        return False
+    else:
+        return True
+
+
+def check_media_types(mediatypes):
+
+    media_type_errors = []
+
+    for index, media_type in enumerate(mediatypes):
+        if media_type is "":
+            media_type_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide a media type for learning resources")
+        else:
+            if media_type not in global_media_types:
+                media_type_errors.append(
+                    f"Line {index + global_header_lines + global_line_offset}: The provided media type is not in the DIF picklist.")
+    return media_type_errors
+
+
+def check_proficiency_levels(proficiency_levels):
+
+    proficiency_level_errors = []
+
+    for index, proficiency_level in enumerate(proficiency_levels):
+        if proficiency_level is "":
+            proficiency_level_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide at least one proficiency level for learning resources.")
+        else:
+            if proficiency_level not in global_proficiency_levels:
+                proficiency_level_errors.append(
+                    f"Line {index + global_header_lines + global_line_offset}: The provided proficiency level is not in the DIF picklist.")
+    return proficiency_level_errors
+
+
+def check_date_format(publication_date):
+    if re.search("^\d{4}-\d{2}-\d{2}$", publication_date) is None and re.search("^\d{4}-\d{2}$", publication_date) is None and re.search("^\d{4}$", publication_date) is None:
+        return True
+    else:
+        return False
+
+
+def check_publication_dates(publication_dates):
+
+    publication_date_errors = []
+
+    for index, publication_date in enumerate(publication_dates):
+        if publication_date is "":
+            publication_date_errors.append(
+                f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide a publication date for learning resources.")
+        else:
+            if check_date_format(publication_date):
+                publication_date_errors.append(
+                    f"Line {index + global_header_lines + global_line_offset}: The provided publication date is not of the format xsd:date."
+                )
+
+    return publication_date_errors
+
+def check_file_format(file_formats):
+    file_formats_list = read_data_formats_file()
+    file_format_errors = []
+
+    for index, file_format in enumerate(file_formats):
+        if file_format is "":
+            file_format_errors.append(f"Line {index + global_header_lines + global_line_offset}: It is recommended to provide the file format of a learning resource.")
+        else:
+            if check_file_format_format(file_format):
+                file_format_errors.append(f"Line {index + global_header_lines + global_line_offset}: The provided file format is not formatted properly.")
+            else:
+                file_format_list = split_into_list(file_format)
+                if check_file_format_list(file_format_list, file_formats_list):
+                    file_format_errors.append(f"Line {index + global_header_lines + global_line_offset}: The provided file format is not included in the picklist.")
+
+
+def check_file_format_format(file_format):
+    if re.search("^\.\w+(?:$|\s\*\s\.\w+)*$", file_format) is None:
+        return True
+    else:
+        return False
+
+
+def check_file_format_list(file_format_list, file_formats):
+    for file_format in file_format_list:
+        if file_format not in file_formats:
+            return True
+    return False
+
 
 def check_data(dataframe):
     # Checks data within a dataframe according to the rules of the DIF.
@@ -251,6 +386,31 @@ def check_data(dataframe):
     else:
         found_errors[
             'Community'] = "It is recommended to provide information about recommending and supporting commnities!"
+
+    if dataframe['Discipline'] is not None:
+        found_errors['Discipline'] = check_disciplines(dataframe['Discipline'])
+    else:
+        found_errors['Discipline'] = "It is recommended to provide at least one relevant discipline listed in https://skohub.io/dini-ag-kim/hochschulfaechersystematik/heads/master/w3id.org/kim/hochschulfaechersystematik/scheme.html"
+
+    if dataframe['MediaType'] is not None:
+        found_errors['MediaType'] = check_media_types(dataframe['MediaType'])
+    else:
+        found_errors['MediaType'] = "It is recommended to provide media types for learning resources."
+
+    if dataframe['ProficiencyLevel'] is not None:
+        found_errors['ProficiencyLevel'] = check_proficiency_levels(dataframe['ProficiencyLevel'])
+    else:
+        found_errors['ProficiencyLevel'] = "It is recommended to provide proficiency levels for learning resources."
+
+    if dataframe['PublicationDate'] is not None:
+        found_errors['PublicationDate'] = check_publication_dates(dataframe['PublicationDate'])
+    else:
+        found_errors['PublicationDate'] = "It is recommended to provide publication dates for learning resources."
+
+    if dataframe['FileFormat'] is not None:
+        found_errors['FileFormat'] = check_file_format(dataframe['FileFormat'])
+    else:
+        found_errors['FileFormat'] = "It is recommended to provide the file formats of the ressources."
 
     return found_errors
 
